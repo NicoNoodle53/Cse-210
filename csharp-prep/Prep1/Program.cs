11541c7c563c80276c26f6e94b1811a3696b044a
niconoodle53 <niconoodle65@gmail.com>
2025-09-18 10:15:16 -0600

    phase 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ask for first and last name
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string Fname = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string Lname = Console.ReadLine();
+
+        //Display name in format given
+        Console.WriteLine($"Your name is {Lname}, {Fname} {Lname}.");
     }
 }
