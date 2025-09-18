ed4653eb50954272e4c306a197343f514cc5ab06
niconoodle53 <niconoodle65@gmail.com>
2025-09-18 10:43:23 -0600

    prepare 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letterGrade;
+        Console.WriteLine("What is your grade? ");
+        string gradeInText = Console.ReadLine();
+        int grade = int.Parse(gradeInText);
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Great job your passing with a {letterGrade}. Keep it up!");
+        }
+        else
+        {
+            Console.WriteLine($"You are currently failing. You have a {letterGrade}. Keep trying you'll get there!");
+        }
     }
 }
