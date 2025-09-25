30fcc6ebd440df98344b7a4327e32aeba3a1f3be
niconoodle53 <niconoodle65@gmail.com>
2025-09-25 10:53:39 -0600

    comments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,9 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        // intialize variable and get random number
         int guess = 0;
         Random randomGenerator = new Random();
         int magic = randomGenerator.Next(1, 100);
+
+        // have them guess till they get it correct
         while (guess != magic)
         {
             Console.WriteLine("What is your guess?");
