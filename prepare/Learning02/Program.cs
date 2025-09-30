95ff53a8335de44d835b29130bc71d7dadfa3661
niconoodle53 <niconoodle65@gmail.com>
2025-09-30 11:06:26 -0600

    Learning02 complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //initialize job1 and assign variables
+        Job Job1 = new Job();
+        Job1._company = "Apple";
+        Job1._jobTitle = "Manager";
+        Job1._startYear = 2021;
+        Job1._endYear = 2023;
+
+        //initialize job2 and assign variables
+        Job Job2 = new Job();
+        Job2._company = "Amazon";
+        Job2._jobTitle = "Software Engineer";
+        Job2._startYear = 2023;
+        Job2._endYear = 2025;
+
+
+        //initialize Resume for Nicholas and assign variables
+        Resume myResume = new Resume();
+        myResume._name = "Nicholas";
+        myResume._jobs.Add(Job1);
+        myResume._jobs.Add(Job2);
+
+        //display info
+        myResume.Display();
     }
 }
